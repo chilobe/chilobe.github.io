2400e2f1f1c57d7b8408bfc7ec47352a9debec9b
Chilobe Mwanza <chilobemwanza@gmail.com>
2019-02-27 18:11:25 +0000

    cookie acceptance mechanism complete

--- scripts/cookiejar.js
+++ scripts/cookiejar.js
@@ -4,9 +4,50 @@
   var $ = window.jQuery;
 
   function CookieJar() {
-  console.log('cookie jar created!');
+    console.log('cookie jar created!');
   }
 
+  CookieJar.prototype.setCookie = function(cname, cvalue, exdays) {
+    console.log('setting cookie "' + cname + '" value="' + cvalue + '" exdays="' + exdays + '"');
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  };
+
+  CookieJar.prototype.getCookie = function(cname) {
+    console.log('getting cookie "' + cname + '"');
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  };
+
+  CookieJar.prototype.getCookie = function(cname) {
+    console.log('getting cookie "' + cname + '"');
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  };
   App.CookieJar = CookieJar;
   window.App = App;
 })(window);
--- scripts/main.js
+++ scripts/main.js
@@ -6,18 +6,37 @@
   var DARK_THEME_CLASS = 'dark-theme';
   var NAV_BAR_DARK_BACKGROUND = 'bg-dark';
   var NAV_BAR_LIGHT_BACKGROUND = 'bg-light';
+  var COOKIE_POLICY_ACCEPTED = 'accept_cookies';
+
   var App = window.App;
   var CookieJar = App.CookieJar;
   var Button = App.Button;
 
 
   var themeToggleButton = new Button(THEME_TOGGLE_BUTTON_SELECTOR);
-  var cookiePolicyAgreeButton = new Button(COOKIE_POLICY_AGREE_BUTTON_SELECTOR);
+
   var cookieJar = new CookieJar();
+
 
 
   /*Check if cookies are enabled*/
 
+  var $cookieAcceptancePopUp = $("<div class='card box-shadow cookie-policy fixed-bottom container'><div class='card-body'><span>Cookies help us deliver our Services. By using our services or clicking I agree, you agree to our use of cookies.&nbsp </span><a href='#'>Learn More</a>&nbsp&nbsp <button type='button' class='btn btn-raised btn-primary' data-button-role='cookie-policy-agree'>I AGREE</button></div></div>")
+  console.log($cookieAcceptancePopUp);
+  if(cookieJar.getCookie(COOKIE_POLICY_ACCEPTED)==='yes')
+  {
+    console.log('cookes accepted!');
+  }
+  else
+  {
+    $("main").append($cookieAcceptancePopUp);
+    var cookiePolicyAgreeButton = new Button(COOKIE_POLICY_AGREE_BUTTON_SELECTOR);
+    cookiePolicyAgreeButton.addClickHandler(function(buttonElement)
+    {
+      cookieJar.setCookie(COOKIE_POLICY_ACCEPTED,'yes',1);
+      $(".cookie-policy").remove();
+    });
+  }
 
   themeToggleButton.addClickHandler(function(buttonElement) {
     var themeToggleSwitch = $(buttonElement).find("i[data-toggle-role='theme-toggle-switch']");
@@ -38,8 +57,6 @@
     }
   });
 
-  cookiePolicyAgreeButton.addClickHandler(function(buttonElement) {
-    var cookiePolicyCard = $(".cookie-policy").remove();
-  });
+
 
 })(window);
